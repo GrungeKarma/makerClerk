5b08aa67c196057ab5d2628b545619b83e059c28
Anthony Lopez <grungekarma@gmail.com>
2021-06-01 17:03:30 -0500

    clean css

--- components/views/About.js
+++ components/views/About.js
@@ -7,7 +7,7 @@ export default () => html`
         Getting together a parts list for a project is hard enough without
         having to enter it into a spreadsheet. Here at makerClerk, you can just
         enter links from Amazon and we will generate a pdf that includes the
-        part name a picture and pricing.
+        part name, a picture, and pricing.
       </p>
     </div>
   </div>
