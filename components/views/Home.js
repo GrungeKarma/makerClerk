e1a5385e954e98362d5cef8eac58ebd5a9f63f57
Anthony Lopez <grungekarma@gmail.com>
2021-06-01 22:56:12 -0500

    refactor css

--- components/views/Home.js
+++ components/views/Home.js
@@ -3,7 +3,10 @@ import html from "html-literal";
 export default () => html`
   <div class="grid-container">
     <div id="meat">
-      <p id="main">Here at makerClerk, we make parts lists from Amazon links</p>
+      <p id="main">
+        Here at makerClerk,<br />
+        we make parts lists from Amazon links
+      </p>
       <div id="quote">
         <p class="quote" id="quoteText"></p>
         <div id="author">
