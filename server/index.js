0f0f1a0ac8affe0ca86aa86fe4b28fd5a32eba18
Anthony Lopez <grungekarma@gmail.com>
2021-06-03 11:43:41 -0500

    create backend server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const myMiddleware = (request, response, next) => {
+  // do something with request and/or response
+  console.log(request.method, request.path);
+  next(); // tell express to move to the next middleware function
+};
+
+app.use(myMiddleware); // use the myMiddleware for every request to the app
+app.use(express.json());
+
+app.route("/**").get((request, response) => {
+  response.status(404).json({ message: "Not Found" });
+});
+
+const PORT = process.env.PORT || 4040;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
